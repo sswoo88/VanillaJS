f758998bd6d1fa835b767402fafdc2b0e100b2ab
sswoo88 <sswoowkd@gmail.com>
2020-07-10 08:37:33 +0900

    Updated operator.js

--- Basic/operator.js
+++ Basic/operator.js
@@ -2,3 +2,61 @@
 console.log('my' + ' cat')
 console.log('1' + 2);
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
+console.log("Shin's \nbook");
+
+// 2. Numeric operators
+
+console.log(1 + 1);  // add
+console.log(1 - 1);  // substract
+console.log(1 / 1);  // divide
+console.log(1 * 1);  // multiply
+console.log(5 % 2);  // remainder
+console.log(2 ** 3);  //exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+const preDecrement = --counter;
+// counter = counter - 1;
+// preDecrement = counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+const postDecrement = counter--;
+// postDecrement = counter;
+// counter = counter + 1;
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >- 6); // greater than or equal
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or)
+console.log(`or: ${value1 || value2 || check()}`);
+
+function check() {
+  for (let i = 0; i < 10; i++){
+    //wasting time
+    console.log('A');
+  }
+  return true;
+}
