1e665c837816e18a2ce565ad865dc102896d989e
sswoo88 <sswoowkd@gmail.com>
2020-07-15 09:06:46 +0900

    solved quiz

--- Basic/operator.js
+++ Basic/operator.js
@@ -170,4 +170,17 @@ for (let i = 0; i < 10; i++) {
 // break, continue
 // Q1. iterate from 0 to 10 and print only even numbers(use continue)
 
+for (let i =0; i < 11; i++) {
+  if ((i % 2) == 0) {
+    console.log(`i: ${i}`);
+  }
+}
+
 // Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+
+for (let i =0; i < 11; i++) {
+  console.log(`i: ${i}`);
+  if ( i == 8 ){
+    break;
+  }
+}
