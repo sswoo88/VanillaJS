c9c70d8fd11bb341ed5708b121aaaa2bf204316b
sswoo88 <sswoowkd@gmail.com>
2020-07-15 21:28:39 +0900

    git commit

--- Basic/basic.js
+++ Basic/basic.js
@@ -10,12 +10,12 @@
 // 2. Variable, rw(read,write)
 // let (added in ES6)
 // mutable data type
-let globalName = 'global name';
+let globalName = "global name";
 {
-    let name = 'Shin';
-    console.log(name);
-    name = 'hello';
-    console.log(name);
+  let name = "Shin";
+  console.log(name);
+  name = "hello";
+  console.log(name);
 }
 console.log(globalName);
 console.log(name);
@@ -23,9 +23,9 @@ console.log(name);
 // var (don't ever use this!)
 // var hoisting (move declaration from bottom to top)
 // has no block scope
-{    
-    age = 4;
-    var age;
+{
+  age = 4;
+  var age;
 }
 console.log(age);
 
@@ -56,15 +56,15 @@ console.log(`value: ${size}, type : ${typeof size}`);
 // number - specific numeric values: infinity, -infinity, NaN
 const infinity = 1 / 0;
 const negativeInfinity = -1 / 0;
-const nAn = 'not a number' / 2;
+const nAn = "not a number" / 2;
 console.log(infinity);
 console.log(negativeInfinity);
 console.log(nAn);
 
 // string
-const char = 'c';
-const brendan = 'brendan';
-const greeting = 'hello ' + brendan;
+const char = "c";
+const brendan = "brendan";
+const greeting = "hello " + brendan;
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
 const helloBob = `hi ${brendan}!`; //template literals (string) => ``
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
@@ -86,29 +86,31 @@ let x = undefined; // or let x;
 console.log(`value: ${x}, type: ${typeof x}`);
 
 // symbol, create unique identifiers for objects
-const symbol1 = Symbol('id');
-const symbol2 = Symbol('id');
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
 console.log(symbol1 === symbol2); //false
-const gSymbol1 = Symbol.for('id');
-const gSymbol2 = Symbol.for('id');
+const gSymbol1 = Symbol.for("id");
+const gSymbol2 = Symbol.for("id");
 console.log(gSymbol1 === gSymbol2); //ture
-console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
+console.log(
+  `value: ${symbol1.description}, type: ${typeof symbol1.description}`
+);
 
 // object, real-life object, data structure
-const shin = { name : 'shin', age : '20'};
+const shin = { name: "shin", age: "20" };
 shin.age = 22;
 console.log(`value: ${shin}, type: ${typeof shin}`);
 console.log(`value: ${shin.age}, type: ${typeof shin.age}`);
 
 // 5. Dynamic typing: dynamically typed language
 
-let text = 'hello';
+let text = "hello";
 console.log(text.charAt(0));
 console.log(`value: ${text}, type: ${typeof text}`);
 text = 1;
 console.log(`value: ${text}, type: ${typeof text}`);
-text = '7' + 5;
+text = "7" + 5;
 console.log(`value: ${text}, type: ${typeof text}`);
-text = '8' / '2';
+text = "8" / "2";
 console.log(`value: ${text}, type: ${typeof text}`);
 //console.log(text.charAt(0)); //runtime error => typescript : TS
--- Basic/operator.js
+++ Basic/operator.js
@@ -1,17 +1,17 @@
 // 1. String concatenation
-console.log('my' + ' cat')
-console.log('1' + 2);
+console.log("my" + " cat");
+console.log("1" + 2);
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
 console.log("Shin's \nbook");
 
 // 2. Numeric operators
 
-console.log(1 + 1);  // add
-console.log(1 - 1);  // substract
-console.log(1 / 1);  // divide
-console.log(1 * 1);  // multiply
-console.log(5 % 2);  // remainder
-console.log(2 ** 3);  //exponentiation
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); //exponentiation
 
 // 3. Increment and decrement operators
 let counter = 2;
@@ -44,7 +44,7 @@ x /= y;
 console.log(10 < 6); // less than
 console.log(10 <= 6); // less than or equal
 console.log(10 > 6); // greater than
-console.log(10 >- 6); // greater than or equal
+console.log(10 > -6); // greater than or equal
 
 // 6. Logical operators: || (or), && (and), ! (not)
 const value1 = false;
@@ -54,9 +54,9 @@ const value2 = 4 < 2;
 console.log(`or: ${value1 || value2 || check()}`);
 
 function check() {
-  for (let i = 0; i < 10; i++){
+  for (let i = 0; i < 10; i++) {
     //wasting time
-    console.log('A');
+    console.log("A");
   }
   return true;
 }
